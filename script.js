0b7160160871615cb18bd098981aeabab2deb527
blue-eyed iguana <61255945+jben817@users.noreply.github.com>
2020-04-04 23:56:34 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var date = moment().format('MMMM Do YYYY');;
+$("#currentDay").text(date);
+
+var time = moment().format('LT');
+$("#currentTime").text(time);
+
+if (time > time) {
+alert("it worked") } 
+
+localStorage.setItem()
