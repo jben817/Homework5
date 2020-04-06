a2d0eb44a59a733326ddaabcd97e6b0bcb586d2c
blue-eyed iguana <61255945+jben817@users.noreply.github.com>
2020-04-05 20:33:00 -0400

    third commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var saveInfoBtn = document.querySelectorAll("#saveInfoBtn");
+var timeBox = document.querySelectorAll("#timeBox");
+var eventInfo = document.querySelectorAll("#eventInfo");
+
 
 var date = moment().format('MMMM Do YYYY');;
 $("#currentDay").text(date);
@@ -5,7 +9,35 @@ $("#currentDay").text(date);
 var time = moment().format('LT');
 $("#currentTime").text(time);
 
-if (time > time) {
-alert("it worked") } 
 
-localStorage.setItem()
+$("#timeBlock" ).change(function() {
+    var present = moment(time).isSame('LT');
+    $("#timeBlock").css("background-color","yellow");
+});
+
+$( "#timeBlock" ).change(function() {
+    var future = moment().isAfter(time);
+    $("#timeBlock").css("background-color","white");
+});
+
+$( "#timeBlock" ).change(function() {
+    var past = moment().isBefore(time);
+    $("#timeBlock").css("background-color","blue");
+});
+
+var eventInfo = document.querySelectorAll("#eventInfo").value;
+
+
+var savedInfo = localStorage.getItem("eventInfo")
+
+$(document).ready(function() {
+
+    $("button").on("click", function() {
+        localStorage.setItem("eventInfo", eventInfo);
+        
+        
+      });
+
+    });
+
+
